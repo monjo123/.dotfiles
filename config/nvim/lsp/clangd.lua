896e43aa1653f5c50210d02599866b1100fcd22e
ocean <wenocean123@gmail.com>
2025-10-17 19:15:13 +0800

    disable clangd snippet

--- config/nvim/lsp/clangd.lua
+++ config/nvim/lsp/clangd.lua
@@ -63,7 +63,7 @@ end
 
 ---@type vim.lsp.Config
 return {
-  cmd = { 'clangd' },
+  cmd = { 'clangd', '--function-arg-placeholders=0' },
   filetypes = { 'c', 'cpp', 'objc', 'objcpp', 'cuda' },
   root_markers = {
     '.clangd',
--- config/nvim/lua/plugins/blink-cmp.lua
+++ config/nvim/lua/plugins/blink-cmp.lua
@@ -47,13 +47,7 @@ return {
     -- Default list of enabled providers defined so that you can extend it
     -- elsewhere in your config, without redefining it, due to `opts_extend`
     sources = {
-      providers = {
-        snippets = {
-          opts = {
-            friendly_snippets = false
-          }
-        }
-      }
+      
     },
  
 
