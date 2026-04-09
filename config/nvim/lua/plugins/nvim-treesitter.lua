541a246c24fe49bdf5b2a16b7d4c7c9c32d9fc8b
ocean <wenocean123@gmail.com>
2026-04-10 02:30:44 +0800

    fix tresitter sometime cant showup

--- config/nvim/lua/plugins/nvim-treesitter.lua
+++ config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,15 +1,13 @@
 -- nvim-treesitter.lua
 vim.pack.add({ "https://github.com/nvim-treesitter/nvim-treesitter" })
-require("nvim-treesitter").install({ "lua", "python", "cpp", "c", "scheme", "latex", "bash", "html" })
+local ensure_install = { "lua", "python", "cpp", "c", "scheme", "latex", "bash", "html" };
+require("nvim-treesitter").install(ensure_install)
 vim.treesitter.language.register("bash", "zsh")
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = { '<filetype>' },
-  callback = function(args)
-    local bufnr = args.buf
-
-    pcall(vim.treesitter.start, bufnr)
-
-    vim.bo[bufnr].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = ensure_install,
+  callback = function() 
+    vim.treesitter.start() 
+    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
   end,
 })
 
