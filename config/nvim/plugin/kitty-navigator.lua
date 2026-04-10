cdd15a9a5c9b697d34423e95313c2119496c65ed
ocean <wenocean123@gmail.com>
2026-04-10 13:20:15 +0800

    move plugins to the plugin

--- config/nvim/plugin/kitty-navigator.lua
+++ config/nvim/plugin/kitty-navigator.lua
@@ -1,4 +1,16 @@
 -- kitty-navigator.lua
+vim.api.nvim_create_autocmd("PackChanged", {
+  callback = function(ev)
+    local name = ev.data.spec.name
+    local kind = ev.data.kind
+
+    if name == "kitty-navigator.nvim" and (kind == "install" or kind == "update") then
+      local kitty_dir = vim.fn.expand("~/.config/kitty/")
+      vim.fn.system({ "cp", "navigate_kitty.py", kitty_dir })
+      vim.fn.system({ "cp", "pass_keys.py", kitty_dir })
+    end
+  end,
+})
 local function kitty()
   if not package.loaded["kitty-navigator"] then
     vim.pack.add({
@@ -16,18 +28,6 @@ local function kitty()
   end
   return require("kitty-navigator")
 end
-vim.api.nvim_create_autocmd("PackChanged", {
-  callback = function(ev)
-    local name = ev.data.spec.name
-    local kind = ev.data.kind
-
-    if name == "kitty-navigator.nvim" and (kind == "install" or kind == "update") then
-      local kitty_dir = vim.fn.expand("~/.config/kitty/")
-      vim.fn.system({ "cp", "navigate_kitty.py", kitty_dir })
-      vim.fn.system({ "cp", "pass_keys.py", kitty_dir })
-    end
-  end,
-})
 vim.keymap.set("n", "<A-h>", function() kitty().navigateLeft() end)
 vim.keymap.set("n", "<A-j>", function() kitty().navigateUp() end)
 vim.keymap.set("n", "<A-k>", function() kitty().navigateDown() end)
