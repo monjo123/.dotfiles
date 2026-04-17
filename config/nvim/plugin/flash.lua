741fd6e30dd4d8864fec6fabf9464a4b943db94f
ocean <wenocean123@gmail.com>
2026-04-17 16:30:30 +0800

    change flash keymap

--- config/nvim/plugin/flash.lua
+++ config/nvim/plugin/flash.lua
@@ -1,5 +1,5 @@
 -- flash.lua
 vim.pack.add({ 'https://github.com/folke/flash.nvim' })
-vim.keymap.set({ 'n', 'x', 'o' }, 's', function() require("flash").jump() end, { silent = true })
+vim.keymap.set({ 'n', 'x', 'o' }, 'z', function() require("flash").jump() end, { silent = true })
 vim.keymap.set({ 'n', 'x', 'o' }, 'S', function() require("flash").treesitter() end, { silent = true })
 vim.keymap.set({ 'n', 'x', 'o' }, 'R', function() require("flash").treesitter_search() end, { silent = true })
