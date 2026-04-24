3aa4c71dcc3a8d4ba57d3aaaa4ca6f892bbc6b85
ocean <wenocean123@gmail.com>
2026-04-24 10:38:41 +0800

    change flash 'z' to '<leader>s'

--- config/nvim/plugin/flash.lua
+++ config/nvim/plugin/flash.lua
@@ -2,6 +2,6 @@
 vim.pack.add({ 'https://github.com/folke/flash.nvim' })
 require("flash").setup()
 
-vim.keymap.set({ 'n', 'x', 'o' }, 'z', function() require("flash").jump() end, { silent = true })
+vim.keymap.set({ 'n', 'x', 'o' }, '<leader>s', function() require("flash").jump() end, { silent = true })
 vim.keymap.set({ 'n', 'x', 'o' }, 'S', function() require("flash").treesitter() end, { silent = true })
 vim.keymap.set({ 'n', 'x', 'o' }, 'R', function() require("flash").treesitter_search() end, { silent = true })
