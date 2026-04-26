5b63eb749d05533bb4381e5a4f5af8d69767b95d
ocean <wenocean123@gmail.com>
2026-04-27 01:39:21 +0800

    add peek for markdown preview

---
+++ config/nvim/plugin/peek.lua
@@ -0,0 +1,21 @@
+-- peek
+vim.api.nvim_create_autocmd('PackChanged', { callback = function(ev)
+  local name, kind = ev.data.spec.name, ev.data.kind
+  if name == 'peek.nvim' and (kind == 'install' or kind == 'update') then
+    if not ev.data.active then vim.cmd.packadd('peek') end
+    vim.system({"deno", "task", "--quiet", "build:fast"}, { cwd = ev.data.path }):wait()
+  end
+end })
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    vim.pack.add({ 'https://github.com/toppair/peek.nvim' })
+    require("peek").setup({
+      app = 'brave'
+    })
+    vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+    vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+  end,
+})
+
