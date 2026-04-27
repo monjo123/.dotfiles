8b8da9f2a19c133a8e610549a7339db252622eb8
ocean <wenocean123@gmail.com>
2026-04-27 22:05:36 +0800

    add github theme when preview markdown

--- config/nvim/plugin/knap.lua
+++ config/nvim/plugin/knap.lua
@@ -1,24 +1,32 @@
 -- knap
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = { 'markdown', 'tex' },
+  pattern = { 'markdown', 'tex', 'html' },
   callback = function()
     vim.pack.add({ 'https://github.com/frabjous/knap' })
     vim.keymap.set( 'n' ,'<leader>p', function() require("knap").toggle_autopreviewing() end)
   end,
 })
 
-vim.g.knap_settings = {
-    htmltohtml = [[A=%outputfile% ; B="${A%.html}-preview.html" ; sed 's/<\/head>/<meta http-equiv="refresh" content="1" ><\/head>/' "$A" > "$B"]],
-    htmltohtmlviewerlaunch = [[A=%outputfile% ; B="${A%.html}-preview.html" ; brave "$B"]],
-    htmltohtmlviewerrefresh = "none",
-
-    mdtohtml = [[A=%outputfile% ; B="${A%.html}-preview.html" ; pandoc --standalone %docroot% -o "$A" && sed 's/<\/head>/<meta http-equiv="refresh" content="1" ><\/head>/' "$A" > "$B"]],
-    mdtohtmlviewerlaunch = [[A=%outputfile% ; brave "${A%.html}-preview.html"]],
-    mdtohtmlviewerrefresh = "none",
-}
+local css_path = vim.fn.expand("~/.config/nvim/static/github-markdown-dark.css")
 
 vim.g.knap_settings = {
+    ["htmltohtml"] = [[A=/tmp/test ; B="${A%.html}-preview.html" ; sed 's/<\/head>/<meta http-equiv="refresh" content="1" ><\/head>/' "$A" > "$B"]],
+    ["htmltohtmlviewerlaunch"] = [[A=/tmp/test ; B="${A%.html}-preview.html" ; brave "$B"]],
+    ["htmltohtmlviewerrefresh"] = "none",
+
+    ["mdtohtml"] = [[A="/tmp/test" ; B="${A%.html}-preview.html" ; ]] ..
+                   [[pandoc --standalone ]] ..
+                   [[--css="]] .. css_path .. [[" ]] ..
+                   [[--variable=header-includes:'<style>body{background-color:#0d1117;margin:0;padding:45px;}</style>' ]] ..
+                   [[--variable=include-before:'<article class="markdown-body">' ]] ..
+                   [[--variable=include-after:'</article>' ]] ..
+                   [[%docroot% -o "$A" && ]] ..
+                   [[sed 's/<\/head>/<meta http-equiv="refresh" content="1" ><\/head>/' "$A" > "$B"]],
+    ["mdtohtmlviewerlaunch"] = [[A=/tmp/test ; brave "${A%.html}-preview.html"]],
+    ["mdtohtmlviewerrefresh"] = "none",
+
     ["textopdfviewerlaunch"] = [[brave %outputfile%]],
     ["textopdfviewerrefresh"] = "none",
     ["textopdfforwardjump"] = [[brave %outputfile%]]
 }
+
