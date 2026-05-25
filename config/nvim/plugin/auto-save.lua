9d6fb9f6328803c39c5da9ac40569aa6e11b8ab6
ocean <wenocean123@gmail.com>
2026-05-25 17:00:58 +0800

    fix the auto-save trigger logic

--- config/nvim/plugin/auto-save.lua
+++ config/nvim/plugin/auto-save.lua
@@ -1,11 +1,26 @@
 -- auto-save
-vim.api.nvim_create_autocmd({ 'InsertLeave', 'TextChanged' }, { once = true, callback = function()
-  local file_path = vim.fn.expand("<afile>:p:h")
-  if vim.fn.isdirectory(file_path) == 0 then
-    vim.fn.mkdir(file_path, "p")
-  end
-  vim.cmd("silent! write")
-  vim.pack.add({ 'https://github.com/pocco81/auto-save.nvim' })
-  require('auto-save').setup()
-end })
+
+-- Attach to the current buffer (0)
+vim.api.nvim_buf_attach(0, false, {
+    on_lines = function(event, buf, changed_tick, firstline, lastline, new_lastline, bytecount)
+        -- This function will trigger on EVERY modification to the buffer,
+        -- including LSP code actions, formatting, and standard typing.
+
+        -- Add your custom logic here
+        print("Buffer " .. buf .. " was modified!")
+    end
+})
+
+vim.api.nvim_create_autocmd("BufModifiedSet", { 
+  once = true, 
+  callback = function()
+    local file_path = vim.fn.expand("<afile>:p:h")
+    if vim.bo.buftype == "" and file_path ~= "" and vim.fn.isdirectory(file_path) == 0 then
+      vim.fn.mkdir(file_path, "p")
+    end
+    vim.cmd("silent! write")
+    vim.pack.add({ 'https://github.com/pocco81/auto-save.nvim' })
+    require('auto-save').setup()
+  end 
+})
 
