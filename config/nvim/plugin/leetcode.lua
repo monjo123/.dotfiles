c8524f5e9df6ab72fe64b80772f50280c0d51752
ocean <wenocean123@gmail.com>
2026-06-26 16:03:00 +0800

    change leetcode dir

--- config/nvim/plugin/leetcode.lua
+++ config/nvim/plugin/leetcode.lua
@@ -29,7 +29,7 @@ if vim.fn.argv(0) == "leetcode.nvim" then
     },
 
     storage = {
-      home = "~/Projects/leetcode",
+      home = "~/leetcode",
       cache = vim.fn.stdpath("cache") .. "/leetcode",
     },
   })
