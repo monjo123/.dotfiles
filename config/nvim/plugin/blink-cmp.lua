5edc9e63a956e9229fb3c960cb30343caeb2f95f
ocean <wenocean123@gmail.com>
2026-09-04 04:30:23 +0800

    delete snippet completion

--- config/nvim/plugin/blink-cmp.lua
+++ config/nvim/plugin/blink-cmp.lua
@@ -32,11 +32,11 @@ vim.api.nvim_create_autocmd('InsertEnter', { once = true, callback = function()
         'fallback'         
       }, 
 
-      ['<Up>'] = { 
+      ['<Right>'] = { 
         'snippet_forward',
         'fallback'      
       },
-      ['<Down>'] = { 
+      ['<Left>'] = { 
         'snippet_backward', 
         'fallback'         
       },
@@ -128,7 +128,7 @@ vim.api.nvim_create_autocmd('InsertEnter', { once = true, callback = function()
     -- Default list of enabled providers defined so that you can extend it
     -- elsewhere in your config, without redefining it, due to `opts_extend`
     sources = {
-      default = { 'lsp', 'path', 'snippets', 'buffer' },
+      default = { 'lsp', 'path', 'buffer' },
     },
 
     -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
